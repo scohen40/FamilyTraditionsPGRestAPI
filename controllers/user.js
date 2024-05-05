01dc472a53b59602fc79f2845856c18b9d44e662
Sarah Cohen <cohen.sarah591@gmail.com>
2024-05-05 19:16:54 -0400

    completed crud operations for the user

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,16 @@
 const User = require('../models/User');
 
-// URL GET + /api/users
+// 1. URL GET + /api/users
 exports.list = async(req, res) => {
     const data = await User.find({});
     res.status(200).json({
         success:true,
-        message:'List of Users',
+        count: data.length,
         data
     });
 }
 
-// URL POST + /api/users
+// 1. URL POST + /api/users
 exports.create = async(req, res) => {
      const data = await User.create(req.body);
      res.status(201).json({
@@ -19,7 +19,7 @@ exports.create = async(req, res) => {
      });
 }
 
-// URL GET + /api/users/:id
+// 3. URL GET + /api/users/:id
 exports.read = async(req, res) => {
     const data = await User.findById(req.params.id);
     res.status(200).json({
@@ -28,13 +28,19 @@ exports.read = async(req, res) => {
     });
 }
 
-// URL PUT + /api/users/:id
+// 4. URL PUT + /api/users/:id
 exports.update = async(req, res) => {
     const data = await User.findByIdAndUpdate(req.params.id, req.body);
-
+    res.status(202).json({
+        success:true,
+        data
+    });
 }
 
-// URL DELETE + /api/users/:id
+// 5. URL DELETE + /api/users/:id
 exports.delete = async(req, res) => {
-    
+    const data = await User.deleteOne({_id:req.params.id});
+    res.status(data.deleteCount ? 204 : 404).json({
+        success: data.deleteCount ? true : false
+    });
 }
